0582b5c427b3ba7535bb948ac372739a4de12077
Vlai22 <cherkovvlad@gmail.com>
2026-02-10 10:57:04 +0400

    GPIO LL test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "stm32g031xx.h" 
 #include "stm32g0xx_ll_system.h"
 #include "stm32g0xx_ll_rcc.h"
+#include "stm32g0xx_ll_bus.h"
+#include "stm32g0xx_ll_gpio.h"
 
 void delay(volatile uint32_t count) {
     while(count--) {
@@ -20,16 +22,19 @@ int main(void) {
     LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
     while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
     LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
-
-    RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
-    // 3. Настраиваем PA4 на выход (Mode: Output)
-    // Регистр MODER: для PA4 это биты 8 и 9. 
-    GPIOA->MODER &= ~(GPIO_MODER_MODE4_Msk); // Сначала очищаем
-    GPIOA->MODER |= (1 << GPIO_MODER_MODE4_Pos); // Устанавливаем 01
+    
+    LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOA);//включаем тактирование gpioa
+    // Установка режима (Input, Output, Alternate Function, Analog)
+    LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_4, LL_GPIO_MODE_OUTPUT);
+    // Установка типа выхода (Push-Pull или Open-Drain)
+    LL_GPIO_SetPinOutputType(GPIOA, LL_GPIO_PIN_4, LL_GPIO_OUTPUT_PUSHPULL);
+    // Установка подтяжки (No Pull, Pull-up, Pull-down)
+    LL_GPIO_SetPinPull(GPIOA, LL_GPIO_PIN_4, LL_GPIO_PULL_UP);
+    // Установка скорости (Low, Medium, High, Very High)
+    LL_GPIO_SetPinSpeed(GPIOA, LL_GPIO_PIN_4, LL_GPIO_SPEED_FREQ_HIGH);
     while(1) {
-        GPIOA->ODR ^= GPIO_ODR_OD4;
-
+        LL_GPIO_TogglePin(GPIOA, LL_GPIO_PIN_4);
         // 5. Ждем. При 64 МГц значение 1 000 000 даст примерно 0.1-0.2 сек
-        delay(6400000);
+        delay(64000000);
     }
 }
